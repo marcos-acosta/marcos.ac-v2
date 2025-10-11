b29c9a723b3bd5bc97d9626f60cb2253e875c2db
marcos-acosta <macosta1625@gmail.com>
2025-10-10 21:16:52 -0400

    Links open in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,22 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
   markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
+        },
+      ],
+    ],
     remarkRehype: {
       footnoteBackContent: "↩\uFE0E", // Use text variation selector to prevent emoji rendering on iOS
     },
