e3f4d63943cb637669b12cfcd8d99ceeb098c6f7
marcos-acosta <macosta1625@gmail.com>
2025-10-10 21:08:11 -0400

    Try modifying remarkRehype to fix emojis on iOS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkRehype: {
+      footnoteBackContent: "↩\uFE0E", // Use text variation selector to prevent emoji rendering on iOS
+    },
+  },
 });
